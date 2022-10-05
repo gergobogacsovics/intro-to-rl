c388240f1d24e74ee60513787a92dc76db8b2cd9
unknown <gergo.bogacsovics@gmail.com>
2022-10-05 10:34:04 +0200

    Added files for Lab 5

---
+++ labs/05 - Unity/Assets/SubmarineController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineController : MonoBehaviour {
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey("w"))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        if (Input.GetKey("d"))
+            transform.Rotate(0.0f, +0.5f, 0.0f);
+
+        if (Input.GetKey("a"))
+            transform.Rotate(0.0f, -0.5f, 0.0f);
+    }
+}
